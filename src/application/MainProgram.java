2f1ccf4626c0ed9e924da747989a921c1b6a30c2
Mateus Cruz <mateus.cruz14m@gmail.com>
2024-08-11 16:06:52 -0300

    insert

--- src/application/MainProgram.java
+++ src/application/MainProgram.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -17,6 +18,8 @@ public class MainProgram {
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
+		System.out.println();
+		
 		System.out.println("=== TEST 2: seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
@@ -24,10 +27,18 @@ public class MainProgram {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== TEST 2: seller findAll ===");
+		System.out.println();
+		
+		System.out.println("=== TEST 3: seller findAll ===");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		System.out.println();
+		
+		System.out.println("=== TEST 4: seller insert ===");
+		Seller newSeller = new Seller(null,"Paul","paul@gmail.com",new Date(),5000.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println(newSeller);
 	}
 }
