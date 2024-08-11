8ba1a507ad1e41f246cdc259c68f7a04e0a9ba02
Mateus Cruz <mateus.cruz14m@gmail.com>
2024-08-11 13:03:55 -0300

    findById

--- src/application/MainProgram.java
+++ src/application/MainProgram.java
@@ -1,28 +1,16 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import db.DB;
-import db.DbIntegrityException;
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class MainProgram {
 
 	public static void main(String[] args) {
 		
-		Department d = new Department(5,"teste");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(d);
-		
-		Seller s = new Seller(1,"name","name@gmail",new Date(),2000.0,d);
-		System.out.println(s);
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 }
